534e332e1f4bc3bfc68bb7412c1615c0676f5da8
Kushal Dotel <kushal.dotel@study.lbef.edu.np>
2024-09-05 09:47:17 +0545

    add stripe package

--- donation/urls.py
+++ donation/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.donation_page, name='my-donation')
+    path('', views.donation_page, name='my-donation'),
+    path('payment-success/', views.payment_success, name='payment-success'),
+    path('payment-failed/', views.payment_failed, name='payment-failed'),
 ]
--- donation/views.py
+++ donation/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 #let's create a donation page
 
 def donation_page(request):
-    pass
+    return render(request, 'donation/donation_page.html')
+
+def payment_success(request):
+    return render(request, 'donation/payment-success.html')
+
+def payment_failed(request):
+
+    return render(request, 'donation/payment-failure.html')
